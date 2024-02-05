4e76e78d3c11a2eb8c353f0a947fb443bc2884e0
Giovanni Ferrami <g.ferrami@gmail.com>
2024-02-05 13:19:22 +1100

    Added citation in __init__.py

--- galess/__init__.py
+++ galess/__init__.py
@@ -2,3 +2,21 @@ __author__ = "Giovanni Ferrami"
 __email__ = "gferrami@student.unimelb.edu.au"
 __version__ = "1.0.0"
 __credits__ = "University of Melbourne"
+__citation__ = """
+% If this work is used to support a publication please
+% cite the following publication:
+% Description of This code
+@ARTICLE{Ferrami_GALESS,
+   author = { {Ferrami}, G. and {Wyithe}, S. }
+    title = "{Awesome sauce code for astronomy projects}",
+  journal = {/mnras},
+ keywords = {},
+     year = 2024,
+    month = feb,
+   volume = 1337,
+    pages = {11-15},
+      doi = {TBA}
+}
+
+% It is also appropriate to link to the following repository: https://github.com/Ferr013/GALESS
+"""
