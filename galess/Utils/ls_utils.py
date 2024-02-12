90e374f2ff150e840d6df9be35d2d82bb0b77a41
Giovanni Ferrami <g.ferrami@gmail.com>
2024-02-12 17:28:04 +1100

    Added docstrings to ls_utils.py

--- galess/Utils/ls_utils.py
+++ galess/Utils/ls_utils.py
@@ -4,16 +4,37 @@ import os
 from importlib.resources import files
 import pickle
 import numpy as np
-
 import galess.LensStat.lens_stat as ls
-
 
 BASEPATH = os.path.dirname(os.path.abspath(__file__)) + '/../data/'
 
 def remove_spaces_from_string(string):
+    '''
+    Returns a string with no spaces.
+
+            Parameters:
+                    input: (string)
+                        String with some spaces
+            Returns:
+                    output: (string)
+                        String with no spaces
+    '''
     return "".join(string.split())
 
 def read_survey_params(title, VERBOSE = 0):
+    '''
+    Returns a dictionary with the input parameters for a given combination of survey and
+    photometric band from a .param file in galess/data/surveys_params/.
+
+            Parameters:
+                    title: (string)
+                        Title of the survey + photo band
+                    VERBOSE: (boolean)
+                        Flag to print the items in params
+            Returns:
+                    params: (dictionary)
+                        Parameters of the requested survey and photometric band
+    '''
     TITLE = remove_spaces_from_string(title)
     FPATH = BASEPATH + 'surveys_params/'+TITLE+'.param'
     params = {}
@@ -30,6 +51,41 @@ def read_survey_params(title, VERBOSE = 0):
     return params
 
 def load_pickled_files(title, DOUBLE_LENS = 0):
+    '''
+    Returns the resulting distributions for a given combination of survey and photometric band.
+    The results has the same data structure as lens_stat.calculate_num_lenses_and_prob().
+
+            Parameters:
+                    title: (string)
+                        Title of the survey + photo band
+                    DOUBLE_LENS: (boolean)
+                        Flag to extract a double lens file structure
+            Returns:
+                    Ngal_matrix_LL: ndarray(dtype=float, ndim=3)
+                        (Model with Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source, integrated
+                        over the source magnitude up to the magnitude limit of the survey.
+                    Theta_E_mat_LL: ndarray(dtype=float, ndim=3)
+                        (Model with Lens Light) Value of the Einstein Radius of a
+                        SIS lens per bin of velocity dispersion, redshift of the
+                        lens and of the source.
+                    Ngal_tensor_LL: ndarray(dtype=float, ndim=4)
+                        (Model with Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source,
+                        and abs magnitude of the source.
+                    Ngal_matrix_noLL: ndarray(dtype=float, ndim=3)
+                        (Model with NO Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source, integrated
+                        over the source magnitude up to the magnitude limit of the survey.
+                    Theta_E_mat_noLL: ndarray(dtype=float, ndim=3)
+                        (Model with NO Lens Light) Value of the Einstein Radius of a
+                        SIS lens per bin of velocity dispersion, redshift of the
+                        lens and of the source.
+                    Ngal_tensor_noLL: ndarray(dtype=float, ndim=4)
+                        (Model with NO Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source,
+                        and abs magnitude of the source.
+    '''
     TITLE = remove_spaces_from_string(title)
     FOLDERPATH = BASEPATH + 'surveys_results/'+TITLE+'/'
     if os.path.exists(FOLDERPATH):
@@ -52,12 +108,11 @@ def load_pickled_files(title, DOUBLE_LENS = 0):
                 FPATH = FOLDERPATH+TITLE+'_Theta_E_noLL_z2.pkl'
                 with open(FPATH, 'rb') as pickle_file: _Theta_E_noLL_z2 = pickle.load(pickle_file)
                 return _temp_LL, _Theta_E_LL_z1, _Theta_E_LL_z2, _prob_LL, _temp_noLL, _Theta_E_noLL_z1, _Theta_E_noLL_z2, _prob_noLL
-            else:
-                FPATH = FOLDERPATH+TITLE+'_Theta_E_LL.pkl'
-                with open(FPATH, 'rb') as pickle_file: _Theta_E_LL = pickle.load(pickle_file)
-                FPATH = FOLDERPATH+TITLE+'_Theta_E_noLL.pkl'
-                with open(FPATH, 'rb') as pickle_file: _Theta_E_noLL = pickle.load(pickle_file)
-                return _temp_LL, _Theta_E_LL, _prob_LL, _temp_noLL, _Theta_E_noLL, _prob_noLL
+            FPATH = FOLDERPATH+TITLE+'_Theta_E_LL.pkl'
+            with open(FPATH, 'rb') as pickle_file: _Theta_E_LL = pickle.load(pickle_file)
+            FPATH = FOLDERPATH+TITLE+'_Theta_E_noLL.pkl'
+            with open(FPATH, 'rb') as pickle_file: _Theta_E_noLL = pickle.load(pickle_file)
+            return _temp_LL, _Theta_E_LL, _prob_LL, _temp_noLL, _Theta_E_noLL, _prob_noLL
     raise ValueError('Files do not exist. Run the model on this survey.')
 
 def save_pickled_files(
@@ -65,6 +120,42 @@ def save_pickled_files(
                     temp_LL, Theta_E_LL, prob_LL,
                     temp_noLL, Theta_E_noLL, prob_noLL,
                     DOUBLE_LENS = 0):
+    '''
+    Saves the resulting distributions for a given combination of survey and photometric band
+    in a picke file inside galess/data/surveys_results/.
+
+            Parameters:
+                    title: (string)
+                        Title of the survey + photo band
+                    Ngal_matrix_LL: ndarray(dtype=float, ndim=3)
+                        (Model with Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source, integrated
+                        over the source magnitude up to the magnitude limit of the survey.
+                    Theta_E_mat_LL: ndarray(dtype=float, ndim=3)
+                        (Model with Lens Light) Value of the Einstein Radius of a
+                        SIS lens per bin of velocity dispersion, redshift of the
+                        lens and of the source.
+                    Ngal_tensor_LL: ndarray(dtype=float, ndim=4)
+                        (Model with Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source,
+                        and abs magnitude of the source.
+                    Ngal_matrix_noLL: ndarray(dtype=float, ndim=3)
+                        (Model with NO Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source, integrated
+                        over the source magnitude up to the magnitude limit of the survey.
+                    Theta_E_mat_noLL: ndarray(dtype=float, ndim=3)
+                        (Model with NO Lens Light) Value of the Einstein Radius of a
+                        SIS lens per bin of velocity dispersion, redshift of the
+                        lens and of the source.
+                    Ngal_tensor_noLL: ndarray(dtype=float, ndim=4)
+                        (Model with NO Lens Light) Number of lensed galaxies per bin
+                        of velocity dispersion, redshift of the lens and the source,
+                        and abs magnitude of the source.
+                    DOUBLE_LENS: (boolean)
+                        Flag to extract a double lens file structure
+            Returns:
+                    None
+    '''
     TITLE = remove_spaces_from_string(title)
     FOLDERPATH = BASEPATH + 'surveys_results/'+TITLE+'/'
     if not os.path.exists(FOLDERPATH):
@@ -104,6 +195,16 @@ def save_pickled_files(
     pass
 
 def print_summary_surveys(surveys_selection):
+    '''
+    Prints a summary of some of the survey parameters and the total number of identifiable lenses
+    for a list of surveys + photo bands.
+
+            Parameters:
+                    title: (list of strings)
+                        Title of the survey + photo band
+            Returns:
+                    None
+    '''
     print(f'|     Survey - Filter     | PSF/Seeing ["] | Area [deg^2] | m_cut [mag] | m_lim [mag] | N [deg^-1] | N_lenses (LL)       |')
     print()
     if hasattr(surveys_selection, '__len__') is False:
